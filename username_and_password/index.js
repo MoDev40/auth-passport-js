67b458c1c3af199d2fe4d8ee36d9a4a14e9fb46b
M40 <modev.404@gmail.com>
2024-07-27 14:23:07 +0300

    store session to the database

--- username_and_password/index.js
+++ username_and_password/index.js
@@ -1,3 +1,4 @@
+import genFunc from "connect-pg-simple"
 import express, { json } from "express"
 import session from "express-session"
 import passport from "passport"
@@ -5,15 +6,25 @@ import userRouter from "./routes/userRoute.js"
 import "./strategy/local-passport.js"
 
 const app = express()
+
 app.use(json())
+
+const PostgresqlStore = genFunc(session);
+const sessionStore = new PostgresqlStore({
+  conString:process.env.DATABASE_URL,
+  tableName:"Session"
+});
+
 app.use(session({
     secret:"secret",
     resave:false,
     saveUninitialized:false,
     cookie:{
         maxAge:1*60*60*1000
-    }
+    },
+    store:sessionStore
 }))
+
 
 app.use(passport.initialize())
 app.use(passport.session())
